b46ae4ea5dc6439ab5babf9fb9fecbc615f9c7f2
SmartRaysam <raysamtob@gmail.com>
2018-09-15 22:04:13 +0100

    implementing onitemclicklistener interface

---
+++ app/src/test/java/com/softdev/smartraysam/carfeeds/PresenterTest.java
@@ -0,0 +1,30 @@
+package com.softdev.smartraysam.carfeeds;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getDataFromURL() {
+    }
+
+    @Test
+    public void onSuccess() {
+    }
+
+    @Test
+    public void onFailure() {
+    }
+}
--- app/src/test/java/com/softdev/smartraysam/carfeeds/getCarFeedTest.java
+++
@@ -1,9 +0,0 @@
-package com.softdev.smartraysam.carfeeds;
-import org.junit.Test;
-public class getCarFeedTest {
-    @Test
-    public void testCarFeed() throws Throwable {
-       //TODO write the logic test code
-    }
-
-}
